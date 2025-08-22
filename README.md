5672ba7a006daea10fecc7bccfe0591016dc5d5a
UmarRauf <umar110302@gmail.com>
2025-08-22 22:26:08 +0800

    Changes to be committed: modified:   README.md new file:   resources/views/hello.blade.php

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 PHP Laravel Project
 Megat's Idea Umar's Sweat haha
 
+Download and create project using composer for all libraries used (as of July 2025)
+
 1. CRUD Blog website
 2. TLS mail service for users (newsletter)
 3. AI chat assistant
 4. Coming soon~ 
+
+DB NEEDS MIGRATION
---
+++ resources/views/hello.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hello Page</title>
+    <link rel="stylesheet" href="https://cdn.simplecss.org/simple.min.css">
+</head>
+<body>
+    <h1>Welcome to My First Laravel Page 🚀</h1>
+    <p>This is a simple page made with Laravel.</p>
+</body>
+</html>
