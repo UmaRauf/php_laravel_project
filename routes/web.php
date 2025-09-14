42e1b2a05d120d2439a206ce3dd497d67e8bcda3
UmarRauf <umar110302@gmail.com>
2025-09-14 16:51:36 +0800

    modified routes/web.php for added pages

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome'); // Laravel's default page
+});
+
+// Your custom page
+Route::get('/hello', function () {
+    return view('hello');
 });
